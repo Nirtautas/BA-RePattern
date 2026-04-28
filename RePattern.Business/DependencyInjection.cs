9ec605f8197f26596966e0aa70ac68aba7dcafbf
Nirtautas Šadauskas <nirtautas.sadauskas@gmail.com>
2026-04-28 11:51:00 +0300

    Config class libraries and packages

---
+++ RePattern.Business/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RePattern.Business
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBusinessServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
---
+++ RePattern.Data/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RePattern.Data
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
